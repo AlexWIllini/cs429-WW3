df8664f036f972ef462ea8b01703f9c3c6dfb176
Alex Weislow <alexander.weislow@gmail.cm>
2017-02-11 16:35:02 -0500

    finished my part of player

--- Player.cs
+++ Player.cs
@@ -2,14 +2,28 @@ using System.Collections.Generic;
 
 public class Player
 {
+    private List<Army> armyList = new List<Army>();
     public List<Army> GetArmies()
     {
-        return null; // TODO: Change
+        int empty = 0;
+        int isEmpty = armyList.Count;
+        if (isEmpty == empty)
+        {
+            return null;
+        }
+        else
+        {
+            return armyList;
+        }
     }
 
     public void AddArmy(Army army, Pos pos)
     {
-        // TODO: Change
+        //create new army object
+        armyList.Add(new Army());
+        //add position and health to latest army object in list
+        armyList[armyList.Count - 1].Position = pos;
+        armyList[armyList.Count - 1].Health = 100;
     }
 
     public void MoveArmy(Army army, Pos toPos)
